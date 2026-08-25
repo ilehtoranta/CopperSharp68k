1f06a60c560c49b0e910aedfbe5626f600383e5d
Ilkka Lehtoranta <ilkka.lehtoranta@valmet.com>
2026-08-25 20:28:03 +0300

    Completed timer.device SDK

---
+++ Sdk.Amiga/TimerDevice/GuestCodecs.cs
@@ -0,0 +1,97 @@
+/*
+- Copyright (C) 2026 Ilkka Lehtoranta
+- SPDX-License-Identifier: MIT
+ */
+
+namespace Amiga;
+
+/// <summary>
+/// Typed big-endian guest-memory boundary for timer.device requests. Algorithms
+/// use <see cref="TimerRequest"/> members; numeric ABI offsets remain here.
+/// </summary>
+public static class TimerRequestCodec
+{
+	public static bool IsMapped<TMemory>(ref TMemory memory, APTR address)
+		where TMemory : struct, IAmigaGuestMemory => address.IsNotNull &&
+		address.Raw <= uint.MaxValue - TimerRequest.Size &&
+		memory.IsMapped(address, TimerRequest.Size);
+
+	public static TimerRequest Read<TMemory>(ref TMemory memory, APTR address)
+		where TMemory : struct, IAmigaGuestMemory => new()
+	{
+		Request = new IORequest
+		{
+			Message = new Message
+			{
+				Node = new Node
+				{
+					Successor = APTR.FromPointer(memory.ReadUInt32(address,
+						ExecLayout.Node.Successor)),
+					Predecessor = APTR.FromPointer(memory.ReadUInt32(address,
+						ExecLayout.Node.Predecessor)),
+					Type = memory.ReadUInt8(address, ExecLayout.Node.Type),
+					Priority = unchecked((sbyte)memory.ReadUInt8(address,
+						ExecLayout.Node.Priority)),
+					Name = STRPTR.FromPointer(memory.ReadUInt32(address,
+						ExecLayout.Node.Name)),
+				},
+				ReplyPort = APTR.FromPointer(memory.ReadUInt32(address,
+					ExecLayout.Message.ReplyPort)),
+				Length = memory.ReadUInt16(address, ExecLayout.Message.Length),
+			},
+			Device = APTR.FromPointer(memory.ReadUInt32(address,
+				ExecLayout.IORequest.Device)),
+			Unit = APTR.FromPointer(memory.ReadUInt32(address,
+				ExecLayout.IORequest.Unit)),
+			Command = (DeviceCommand)memory.ReadUInt16(address,
+				ExecLayout.IORequest.Command),
+			Flags = (IOFlags)memory.ReadUInt8(address, ExecLayout.IORequest.Flags),
+			Error = unchecked((sbyte)memory.ReadUInt8(address,
+				ExecLayout.IORequest.Error)),
+		},
+		Time = new TimeVal
+		{
+			Seconds = memory.ReadUInt32(address,
+				TimerDeviceLayout.TimerRequest.Seconds),
+			Microseconds = memory.ReadUInt32(address,
+				TimerDeviceLayout.TimerRequest.Microseconds),
+		},
+	};
+
+	public static void Write<TMemory>(ref TMemory memory, APTR address,
+		TimerRequest value) where TMemory : struct, IAmigaGuestMemory
+	{
+		memory.WriteUInt32(address, ExecLayout.Node.Successor,
+			value.Request.Message.Node.Successor.Raw);
+		memory.WriteUInt32(address, ExecLayout.Node.Predecessor,
+			value.Request.Message.Node.Predecessor.Raw);
+		memory.WriteUInt8(address, ExecLayout.Node.Type,
+			value.Request.Message.Node.Type);
+		memory.WriteUInt8(address, ExecLayout.Node.Priority,
+			unchecked((byte)value.Request.Message.Node.Priority));
+		memory.WriteUInt32(address, ExecLayout.Node.Name,
+			value.Request.Message.Node.Name.Raw);
+		memory.WriteUInt32(address, ExecLayout.Message.ReplyPort,
+			value.Request.Message.ReplyPort.Raw);
+		memory.WriteUInt16(address, ExecLayout.Message.Length,
+			value.Request.Message.Length);
+		memory.WriteUInt32(address, ExecLayout.IORequest.Device,
+			value.Request.Device.Raw);
+		memory.WriteUInt32(address, ExecLayout.IORequest.Unit,
+			value.Request.Unit.Raw);
+		memory.WriteUInt16(address, ExecLayout.IORequest.Command,
+			(ushort)value.Request.Command);
+		memory.WriteUInt8(address, ExecLayout.IORequest.Flags,
+			(byte)value.Request.Flags);
+		memory.WriteUInt8(address, ExecLayout.IORequest.Error,
+			unchecked((byte)value.Request.Error));
+		memory.WriteUInt32(address, TimerDeviceLayout.TimerRequest.Seconds,
+			value.Time.Seconds);
+		memory.WriteUInt32(address, TimerDeviceLayout.TimerRequest.Microseconds,
+			value.Time.Microseconds);
+	}
+
+	public static APTR ReadDevice<TMemory>(ref TMemory memory, APTR address)
+		where TMemory : struct, IAmigaGuestMemory => APTR.FromPointer(
+		memory.ReadUInt32(address, ExecLayout.IORequest.Device));
+}
--- Sdk.Amiga/TimerDevice/TimerDevice.cs
+++ Sdk.Amiga/TimerDevice/TimerDevice.cs
@@ -13,6 +13,9 @@ using CopperSharp.Sdk.Amiga;
 public static class TimerDevice
 {
 	public const string Name = "timer.device";
+	public const short AddTimeLvo = -42;
+	public const short SubTimeLvo = -48;
+	public const short CmpTimeLvo = -54;
 	public const short Open = -6;
 	public const short Close = -12;
 	public const short Expunge = -18;
@@ -20,11 +23,36 @@ public static class TimerDevice
 	public const short BeginIO = -30;
 	public const short AbortIO = -36;
 	public const short ReadEClockLvo = -60;
+	public const short GetSysTimeLvo = -66;
 	public const uint UnitEClock = (uint)TimerUnit.EClock;
+
+	[AmigaLvo(AddTimeLvo)]
+	public static extern void AddTime(
+		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
+		[M68kRegister(M68kRegister.A0)] APTR destination,
+		[M68kRegister(M68kRegister.A1)] APTR source);
+
+	[AmigaLvo(SubTimeLvo)]
+	public static extern void SubTime(
+		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
+		[M68kRegister(M68kRegister.A0)] APTR destination,
+		[M68kRegister(M68kRegister.A1)] APTR source);
+
+	[AmigaLvo(CmpTimeLvo)]
+	[return: M68kRegister(M68kRegister.D0)]
+	public static extern int CmpTime(
+		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
+		[M68kRegister(M68kRegister.A0)] APTR destination,
+		[M68kRegister(M68kRegister.A1)] APTR source);
 
 	[AmigaLvo(ReadEClockLvo)]
 	[return: M68kRegister(M68kRegister.D0)]
 	public static extern uint ReadEClock(
 		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
 		[M68kRegister(M68kRegister.A0)] APTR value);
+
+	[AmigaLvo(GetSysTimeLvo)]
+	public static extern void GetSysTime(
+		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
+		[M68kRegister(M68kRegister.A0)] APTR destination);
 }
