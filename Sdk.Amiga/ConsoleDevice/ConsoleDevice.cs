69c1d925bf64cd6638ffecc0856f5a97fb975d75
Ilkka Lehtoranta <ilkka.lehtoranta@valmet.com>
2026-08-25 20:29:13 +0300

    Added RawKeyConvert

--- Sdk.Amiga/ConsoleDevice/ConsoleDevice.cs
+++ Sdk.Amiga/ConsoleDevice/ConsoleDevice.cs
@@ -1,11 +1,25 @@
+using CopperSharp.Compiler;
+using CopperSharp.Sdk.Amiga;
+
 namespace Amiga;
 
+[AmigaLibrary(Name, AmigaLibraryBasePolicy.CallerProvided)]
 public static class ConsoleDevice
 {
 	public const string Name = "console.device";
 	public const short Open = -6, Close = -12, Expunge = -18, ExtFunc = -24,
 		BeginIO = -30, AbortIO = -36, CDInputHandler = -42, RawKeyConvert = -48;
 	public const int DefaultColumns = 80, DefaultRows = 25;
+
+	/// <summary>Calls the MorphOS <c>RawKeyConvert</c> device vector.</summary>
+	[AmigaLvo(RawKeyConvert)]
+	[return: M68kRegister(M68kRegister.D0)]
+	public static extern int ConvertRawKey(
+		[M68kRegister(M68kRegister.A6)] APTR deviceBase,
+		[M68kRegister(M68kRegister.A0)] APTR inputEvent,
+		[M68kRegister(M68kRegister.A1)] APTR buffer,
+		[M68kRegister(M68kRegister.D1)] int length,
+		[M68kRegister(M68kRegister.A2)] APTR keyMap);
 }
 
 public enum ConsoleCommand : ushort
